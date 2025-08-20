7f3ad62e55a4731bbd264d88602f9d214e2d0b55
Ilirjan Rexho <61479363+ilrexho2011@users.noreply.github.com>
2025-08-20 14:00:22 +0200

    Update email.php

--- aplikacioni/config/email.php
+++ aplikacioni/config/email.php
@@ -8,21 +8,22 @@ $config['email_config'] = array();
 // 'smtp_crypto' => 'ssl',
 // 'smtp_host' => 'smtp.gmail.com',
 // 'smtp_port' => '587',
-// 'smtp_user' => 'ilreincpi@gmail.com',
-// 'smtp_pass' => 'ijymoduwdjmxatyz', // api pass 
+// 'smtp_user' => 'abcdefghijkl@gmail.com',
+// 'smtp_pass' => 'yyyyyyyyyyyyyyyyyyy', // api pass 
 // 'charset' => 'utf-8',
 // 'newline' => '\r\n',
 
 // $config['protocol'] = 'smtp';
 // $config['smtp_host'] = 'smtp.gmail.com';
 // $config['smtp_port'] = 587; // 465 if using SSL
-// $config['smtp_user'] = 'ilreincpi@gmail.com';
-// $config['smtp_pass'] = 'ijymoduwdjmxatyz'; // NOT your Gmail password, use App Password
+// $config['smtp_user'] = 'abcdefghijklmn@gmail.com';
+// $config['smtp_pass'] = 'yyyyyyyyyyyyyyyyyyyyy'; // NOT your Gmail password, use App Password
 // $config['smtp_crypto'] = 'tls'; // ssl for port 465
 // $config['mailtype'] = 'html';
 // $config['charset'] = 'utf-8';
 // $config['wordwrap'] = TRUE;
 // $config['newline'] = "\r\n"; // very important for SMTP
 // $config['crlf'] = "\r\n";    // very important for SMTP
+
 
 ?>
